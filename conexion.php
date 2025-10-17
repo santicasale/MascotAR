16f800b41622718f8a206a009968cd33afdda8cb
Franco Dionisio <dionisiofranco8@gmail.com>
2025-10-17 11:31:13 -0300

    ultima modificación

--- conexion.php
+++ conexion.php
@@ -1,12 +1,11 @@
 <?php
-$servername = "sql313.infinityfree.com";
-$username = "if0_40059520"; 
-$password = "MightyNo9";     
-$dbname = "if0_40059520_mascotar";
+$servername = "localhost";
+$username = "root"; 
+$password = "";     
+$dbname = "MascotAR";
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Verificar
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
--- config.php
+++ config.php
@@ -1,20 +1,19 @@
 <?php
 // config.php
 // === CONFIGURACIÓN DE BASE DE DATOS ===
-define('DB_HOST', 'sql313.infinityfree.com');
-define('DB_USER', 'if0_40059520');
-define('DB_PASS', 'MightyNo9');
-define('DB_NAME', 'if0_40059520_mascotar');  
+define('DB_HOST', 'localhost');  
+define('DB_USER', 'root');       
+define('DB_PASS', '');           
+define('DB_NAME', 'MascotAR');   
 
 // === CONFIGURACIÓN DE MERCADO PAGO ===
 // Acces Token de pruebas o producción
 define('MP_ACCESS_TOKEN', 'APP_USR-502327826682038-100521-ae23557030dcf30365aa3fa312265775-2906214560');
 
 // === CONFIGURACIÓN DE RUTAS ===
-define('BASE_URL', 'https://mascot-ar.kesug.com');
+define('BASE_URL', 'http://localhost/MascotAR'); 
 define('WEBHOOK_URL', BASE_URL . '/webhook.php');
-
 // === OPCIONAL ===
 error_reporting(E_ALL);
-ini_set('display_errors', 0); // 0 = No mostrar en pantalla, 1 = mostrar errores
+ini_set('display_errors', 1); // 0 = No mostrar en pantalla, 1 = mostrar errores
 ?>
