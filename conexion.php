783ea7a3821b8b2474478d47ffb7ee23f8584e81
Franco Dionisio <dionisiofranco8@gmail.com>
2025-11-13 13:13:35 -0300

    adoptar

--- conexion.php
+++ conexion.php
@@ -1,14 +1,12 @@
 <?php
-$servername = "sql111.infinityfree.com";
-$username = "if0_40132447";
-$password = "1zY0LORQz4gMI4";
-$dbname = "if0_40132447_mascotar";
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "mascotar";
 
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-$conn->set_charset("utf8mb4");
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-} 
-?>
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
+    }
+    ?>
